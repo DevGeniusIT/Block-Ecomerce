c6b0570526de32dc83a4524ff2a4ef2d7540a5ef
DevGeniusIT <daingostmtp@gmail.com>
2024-01-16 23:17:33 +0700

    email auth

--- api/src/services/emailService.js
+++ api/src/services/emailService.js
@@ -4,8 +4,8 @@ const nodemailer = require("nodemailer");
 let sendSimpleEmail = async (dataSend) => {
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        port: 465,
+        secure: true, // true for 465, false for other ports
         auth: {
             user: process.env.EMAIL_APP,
             pass: process.env.EMAIL_APP_PASSWORD,
@@ -13,17 +13,17 @@ let sendSimpleEmail = async (dataSend) => {
     });
     if (dataSend.type === 'verifyEmail') {
         let info = await transporter.sendMail({
-            from: '"BiNgo2706 👻" <dotanthanhvlog@gmail.com>', // sender address
+            from: '"Area-Shop 👻" <ndngo.19it5@vku.udn.vn>', // sender address
             to: dataSend.email, // list of receivers
-            subject: "Xác thực email | PTITSHOP", // Subject line
+            subject: "Xác thực email | AREASHOP", // Subject line
             html: getBodyHTMLEmailVerify(dataSend)
         });
     }
     if (dataSend.type === 'forgotpassword') {
         let info = await transporter.sendMail({
-            from: '"BiNgo2706 👻" <dotanthanhvlog@gmail.com>', // sender address
+            from: '"Area-Shop 👻" <ndngo.19it5@vku.udn.vn>', // sender address
             to: dataSend.email, // list of receivers
-            subject: "Xác nhận quên mật khẩu | PTITSHOP", // Subject line
+            subject: "Xác nhận quên mật khẩu | AREASHOP", // Subject line
             html: getBodyHTMLEmailForgotPassword(dataSend)
         });
     }
@@ -32,7 +32,7 @@ let getBodyHTMLEmailVerify = (dataSend) => {
     let fullname = `${dataSend.firstName} ${dataSend.lastName}`
     let result = `<h3>Xin chào ${fullname}!</h3>
         <p>Bạn nhận được email này vì đã thực hiện lệnh xác thực email!</p>
-        <p>Bui lòng click vào đường link bên dưới để xác nhận và hoàn tất thủ tục xác minh email của bạn</p>
+        <p>Vui lòng click vào đường link bên dưới để xác nhận và hoàn tất thủ tục xác minh email của bạn</p>
         <div>
             <a href="${dataSend.redirectLink}" target=""_blank>Click here</a>
         </div>
@@ -45,7 +45,7 @@ let getBodyHTMLEmailForgotPassword = (dataSend) => {
     let fullname = `${dataSend.firstName} ${dataSend.lastName}`
     let result = `<h3>Xin chào ${fullname}!</h3>
         <p>Bạn nhận được email này vì đã thực hiện lệnh quên mật khẩu!</p>
-        <p>Bui lòng click vào đường link bên dưới để xác nhận quên mật khẩu và lấy lại mật khẩu của bạn</p>
+        <p>Vui lòng click vào đường link bên dưới để xác nhận quên mật khẩu và lấy lại mật khẩu của bạn</p>
         <div>
             <a href="${dataSend.redirectLink}" target=""_blank>Click here</a>
         </div>
